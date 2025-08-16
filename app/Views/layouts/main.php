9531afda7657997797880a2b13263bdfddd50cc1
Devendra Sonawane <dpsonawane789@gmail.com>
2025-08-16 11:33:04 +0530

    minor changes in UI

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -6,6 +6,8 @@
     <title>The Code Cafe</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #4a3f35;">
